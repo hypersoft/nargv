65d5b93dcb270f20e58980838437ac3049354f0f
Triston J. Taylor <pc.wiz.tt@gmail.com>
2013-01-07 03:52:51 -0600

    Removed header file
    moving to own file

--- nargv.c
+++ nargv.c
@@ -13,22 +13,6 @@
 // Public Type Structs: NARGV
 // Public Data: None
 
-/* nargv.h:
-
-#include <stddef.h>
-
-typedef struct NARGV {
-    char **argv, *data, *error_message;
-    int argc, data_length, error_index, error_code;
-} NARGV;
-
-void nargv_free(NARGV* props);
-void nargv_ifs(char *nifs);
-NARGV *nargv_parse(char *input)
-
-// end-of-file
-
-*/ 
 //============================================================================
 
 #include <stddef.h>
